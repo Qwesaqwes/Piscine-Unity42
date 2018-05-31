952c04128618ccecb4bfe46d268eea21f8ba6dea
Jimmy CHEN-MA <jichen-m@e3r10p22.42.fr>
2018-06-01 00:56:06 +0200

    first commit

---
+++ d03/Assets/Scripts/ex00/Button.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+	public void ChangeScene (int scene)
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+}
