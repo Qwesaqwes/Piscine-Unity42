59dc9bd5e25ca852f2363a6b18b6447545d23c21
Jimmy CHEN-MA <jichen-m@e3r8p21.42.fr>
2018-06-01 22:23:31 +0200

    first commit

---
+++ d03/Assets/Scripts/ex01/ItemDragHandler.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ItemDragHandler : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+	private	Vector3		    _target;
+	private	Vector3		    _endDragPos;
+    public SpriteRenderer   RangeImage;
+    public GameObject       Tower;
+    private towerScript     _towerScript;
+    [HideInInspector] public int energyOfTower = 0;
+    [HideInInspector] public bool enableDrag = true;       
+   	// private Text		_textEnergy;
+  
+
+    // Use this for initialization
+    void Start ()
+	{
+		_target = transform.position;
+        // _textEnergy = GameObject.Find("TextEnergy").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+	 public void OnDrag(PointerEventData eventData)
+    {
+        if (enableDrag)
+        {
+            _target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            _target.z = transform.position.z;
+            RangeImage.enabled = true;
+            RangeImage.transform.position = _target;
+            transform.position = _target;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        if (hit.collider != null && enableDrag)
+        { 
+            _endDragPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            _endDragPos.z = _target.z;
+            Tower.transform.position = _endDragPos;
+            Instantiate(Tower, Tower.transform.position, Quaternion.identity);
+            towerScript _towerScript = Tower.GetComponent<towerScript>();
+            energyOfTower = _towerScript.energy;
+        }
+        transform.localPosition = Vector3.zero;
+        RangeImage.transform.position = Vector3.zero;
+        RangeImage.enabled = false;
+    }
+}
---
+++ d03/Assets/Scripts/ex01/UIManager.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+	private GameObject	hp;
+	private	gameManager	_player;
+	private	Text		_textLife;
+	private Text		_textEnergy;
+	private ItemDragHandler	_itemDragedTower1;
+	private ItemDragHandler	_itemDragedTower2;
+	private ItemDragHandler	_itemDragedTower3;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_player = GameObject.Find("GameManager").GetComponent<gameManager>();
+		_textLife = GameObject.Find("TextLife").GetComponent<Text>();
+		_textEnergy = GameObject.Find("TextEnergy").GetComponent<Text>();
+		_itemDragedTower1 = GameObject.Find("ImageTower1").GetComponent<ItemDragHandler>();
+		_itemDragedTower2 = GameObject.Find("ImageTower2").GetComponent<ItemDragHandler>();
+		_itemDragedTower3 = GameObject.Find("ImageTower3").GetComponent<ItemDragHandler>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		_textLife.text = _player.playerHp.ToString();
+		checkTowerUP();
+		if (checkEnergyForTower() == 1)
+		{
+			energyDrain();
+			_textEnergy.text = _player.playerEnergy.ToString();
+		}
+	}
+
+	void checkTowerUP()
+	{
+		if (_player.playerEnergy >= 100)
+		{
+			_itemDragedTower1.GetComponent<Image>().color = new Color32(255, 255, 255, 100);
+			_itemDragedTower1.enableDrag = true;
+			_itemDragedTower2.GetComponent<Image>().color = new Color32(255, 255, 255, 100);
+			_itemDragedTower2.enableDrag = true;
+			_itemDragedTower3.GetComponent<Image>().color = new Color32(255, 255, 255, 100);
+			_itemDragedTower3.enableDrag = true;
+		}
+		if (_player.playerEnergy >= 80)
+		{
+			_itemDragedTower1.GetComponent<Image>().color = new Color32(255, 255, 255, 100);
+			_itemDragedTower1.enableDrag = true;
+			_itemDragedTower2.GetComponent<Image>().color = new Color32(255, 255, 255, 100);
+			_itemDragedTower2.enableDrag = true;
+		}
+		if (_player.playerEnergy >= 50)
+		{
+			_itemDragedTower2.GetComponent<Image>().color = new Color32(255, 255, 255, 100);
+			_itemDragedTower2.enableDrag = true;
+		}
+	}
+
+	int checkEnergyForTower()
+	{
+			// Debug.Log(_itemDragedTower1.energyOfTower);
+		if (_player.playerEnergy < 50)
+		{
+			_itemDragedTower1.GetComponent<Image>().color = new Color32(255, 0, 0, 100);
+			_itemDragedTower1.enableDrag = false;
+			_itemDragedTower2.GetComponent<Image>().color = new Color32(255, 0, 0, 100);
+			_itemDragedTower2.enableDrag = false;
+			_itemDragedTower3.GetComponent<Image>().color = new Color32(255, 0, 0, 100);
+			_itemDragedTower3.enableDrag = false;
+			return 0;
+		}
+		if (_player.playerEnergy < 80)
+		{
+			_itemDragedTower1.GetComponent<Image>().color = new Color32(255, 0, 0, 100);
+			_itemDragedTower1.enableDrag = false;
+			_itemDragedTower3.GetComponent<Image>().color = new Color32(255, 0, 0, 100);
+			_itemDragedTower3.enableDrag = false;
+			return 0;
+		}
+		if (_player.playerEnergy < 100)
+		{
+			_itemDragedTower3.GetComponent<Image>().color = new Color32(255, 0, 0, 100);
+			_itemDragedTower3.enableDrag = false;
+			return 0;
+		}
+		
+		// if (_itemDragedTower1.energyOfTower > _player.playerEnergy)
+		// {
+		// 	_itemDragedTower1.GetComponent<Image>().color = new Color32(255, 0, 0, 100);
+		// 	_itemDragedTower1.enableDrag = false;
+		// 	_itemDragedTower3.GetComponent<Image>().color = new Color32(255, 0, 0, 100);
+		// 	_itemDragedTower3.enableDrag = false;
+			
+		// 	return 0;
+		// }
+		// if (_itemDragedTower2.energyOfTower > _player.playerEnergy)
+		// {
+		// 	_itemDragedTower1.GetComponent<Image>().color = new Color32(255, 0, 0, 100);
+		// 	_itemDragedTower1.enableDrag = false;
+		// 	_itemDragedTower2.GetComponent<Image>().color = new Color32(255, 0, 0, 100);
+		// 	_itemDragedTower2.enableDrag = false;
+		// 	_itemDragedTower3.GetComponent<Image>().color = new Color32(255, 0, 0, 100);
+		// 	_itemDragedTower3.enableDrag = false;
+		// 	return 0;
+		// }
+		// if (_itemDragedTower3.energyOfTower > _player.playerEnergy)
+		// {
+		// 	_itemDragedTower3.GetComponent<Image>().color = new Color32(255, 0, 0, 100);
+		// 	_itemDragedTower3.enableDrag = false;
+		// 	return 0;
+		// }
+		return 1;
+	}
+
+	void energyDrain()
+	{
+		if (_itemDragedTower1.energyOfTower != 0)
+		{
+			_player.playerEnergy -= _itemDragedTower1.energyOfTower;	
+			_itemDragedTower1.energyOfTower = 0;
+			// Debug.Log(_itemDragedTower1.energyOfTower);
+		}
+		else if (_itemDragedTower2.energyOfTower != 0)
+		{
+			_player.playerEnergy -= _itemDragedTower2.energyOfTower;	
+			_itemDragedTower2.energyOfTower = 0;
+		}
+		else if (_itemDragedTower3.energyOfTower != 0)
+		{
+			_player.playerEnergy -= _itemDragedTower3.energyOfTower;	
+			_itemDragedTower3.energyOfTower = 0;
+		}
+	}
+}
